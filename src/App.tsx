a3932d3e35573c247782b27f6d6066f22ec627b1
education-bootcamp <education.seekerscloud@gmail.com>
2023-12-13 20:38:57 +0530

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
 import './App.css'
 
 function App() {
-
-
   return (
-   <h1>Hello Team!</h1>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/customer'>Customer</Link>
+            </li>
+            <li>
+              <Link to='/product'>Product</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr/>
+
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/customer' element={<Customer/>}/>
+          <Route path='/product' element={<Product/>}/>
+        </Routes>
+
+      </div>
+    </Router>
   )
 }
 
